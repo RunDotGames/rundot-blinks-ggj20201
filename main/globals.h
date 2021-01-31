648005df9a8660869f136cb7d83e86a410754754
Mark Mayer <mdm373@gmail.com>
2021-01-31 12:18:59 -0500

    hard mode is best mode

--- main/globals.h
+++ main/globals.h
@@ -1,12 +1,22 @@
 #ifndef HEADER_GLOBALS
     #define HEADER_GLOBALS
-    
-    //toggle comment for debug/normal view
-    // #define GLOBALS_DEBUG 
 
+    #include "blink-types.h"
+    
     #define GLOBALS_SLOW_PULSE 16
     #define GLOBALS_FAST_PULSE 4
     #define GLOBALS_FAST_SPIN 4
     #define GLOBALS_MESSAGE_DELAY 200
+    
+    #define GLOBALS_DISPLAY_MODE_STANDARD 0
+    #define GLOBALS_DISPLAY_MODE_HARD 1
+    #define GLOBALS_DISPLAY_MODE_DEBUG 2
+    #define GLOBALS_DISPLAY_MODE_COUNT 3
+
+    namespace globals {
+        void setDisplayMode(byte displayMode);
+        byte getDisplayMode();
+    }
+    
     
 #endif
